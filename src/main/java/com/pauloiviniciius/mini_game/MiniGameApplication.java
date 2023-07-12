f7f885c269c6b0539ea02777092fa4e70c125157
PAULO VINICIUS SILVA COSTA <paulocosta@mv.com.br>
2023-07-12 14:36:56 -0300

    first commit

---
+++ src/main/java/com/pauloiviniciius/mini_game/MiniGameApplication.java
@@ -0,0 +1,13 @@
+package com.pauloiviniciius.mini_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pauloiviniciius/mini_game/MiniGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloiviniciius.mini_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
